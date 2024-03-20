60dd96ff6c48b41144cf69ce9d79674298316c07
Riley <roverlan@uwo.ca>
2024-03-20 02:27:42 -0400

    fixed a solution

---
+++ neetcode/linkedList/LRU_cache/LRU.js
@@ -0,0 +1,62 @@
+// Design a data structure that follows the constraints of a Least 
+// Recently Used (LRU) cache.
+
+// Implement the LRUCache class:
+
+// LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+// int get(int key) Return the value of the key if the key exists, otherwise return -1.
+// void put(int key, int value) Update the value of the key if the key exists. 
+// Otherwise, add the key-value pair to the cache. If the number of keys 
+// exceeds the capacity from this operation, evict the least recently used key.
+// The functions get and put must each run in O(1) average time complexity.
+
+
+
+
+//this solution is much easier in js beacuse we can utilize the map.delete function
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    //create a map to store key:value pairs
+    this.map = new Map()
+    this.capacity = capacity
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    //check if we dont have the key in the map,return -1 if we dont
+    if(!this.map.has(key))return -1
+    const val = this.map.get(key)
+    //we are going to update the map and replace the value in it so it is the most
+    //recently used key, we need to delete the old version first them re-add
+    this.map.delete(key)
+    this.map.set(key,val)
+    return val
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    //if the key exists already delete it
+    this.map.delete(key)
+    this.map.set(key,value)
+    //check if the size is greater than capacty and reduce it if thats true
+    if(this.map.size > this.capacity){
+        const lastUsed = this.map.keys().next.value
+        this.map.delete(lastUsed)
+    }
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
--- neetcode/linkedList/findDuplicateNumber/dup.js
+++ neetcode/linkedList/findDuplicateNumber/dup.js
@@ -11,15 +11,55 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findDuplicate = function(nums) {
+var findDuplicateExtraSpace = function(nums) {
     //since we know the array is larger than the number of possible values,
     //there must exist a duplicate
+    //use a set to keep track of every element we see
     let set = new Set()
     for(let i = 0;i<nums.length;i++){
         if(set.has(nums[i]))return nums[i]
         set.add(nums[i])
     }
 };
+//we need to only use constant extra space so this solution is not correct
+
+
+//this is called floyds algo, and we can use a linked list to find the cycle, which will 
+//be out duplicate number. First we need to implement a fast and slow pointer, once we
+//find the interesection, the duplicate will be equal distance from the original start
+//of the array so we can just decrease from intersection and increase the start 
+//until they both meet
+//we will treat the array as each value points to that specfic element
+//so number 3 points to the third index, since the range is 1-n we know there will be
+//no number that points to 0 so we start at 0
+var findDuplicate = function(nums){
+    let slow = 0, fast = 0
+
+    do{
+        //iterate slow and fast until they intersect
+        slow = nums[slow]
+        //since we need to iterate twice just take the value twice
+        fast = nums[nums[fast]]
+    }while(slow != fast)
+    // we have the intersection
+    //now count until we intersect
+    let slow2 = 0
+    while(true){
+        //increment from the start until the are equal
+        slow = nums[slow]
+        slow2 = nums[slow2]
+        if(slow == slow2)return slow
+    }
+}
+
+
+
+
+
+console.log(findDuplicateExtraSpace([1,3,4,2,2]))
+console.log(findDuplicateExtraSpace([3,1,3,4,2]))
+console.log(findDuplicateExtraSpace([3,3,3,3,3]))
+console.log("/////////////////////////////////")
 console.log(findDuplicate([1,3,4,2,2]))
 console.log(findDuplicate([3,1,3,4,2]))
 console.log(findDuplicate([3,3,3,3,3]))
